a54c5ca12b2f70ca5d46cebaaea9b82e2bc85f11
Elyas Dwi Maryanto <elyasdwimaryanto27@gmail.com>
2018-12-13 11:17:17 +0700

    ngekost

---
+++ pages/about.php
@@ -0,0 +1,9 @@
+<div class="about">
+    <h1>about</h1>		
+    <div>
+        <p>Web ini dibuat untuk memenuhi tugas kuliah Pemrograman Web.</p>
+		<p>Nama         : Elyas Dwi Maryanto</p>
+        <p>NPM          : 1615061005</p>
+        <p>Prodi        : Teknik Informatika 2016</p>
+    </div>
+</div>
